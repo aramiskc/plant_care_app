d6dbbaf624ae2b72c9c2597bb1d506fba480440e
“Nickhil <suryanscrit@gmail.com>
2023-09-05 15:05:10 -0500

    controllers finished

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,42 @@
 class PlantsController < ApplicationController
+  class PlantsController < ApplicationController
+    def index
+      @plants = Plant.all
+      render json: @plants
+    end
+
+    def show
+      @plant = Plant.find(params[:id])
+      render json: @plant
+    end
+
+    def create
+      @plant = Plant.new(plant_params)
+      if @plant.save
+        render json: @plant, status: :created
+      else
+        render json: @plant.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @plant = Plant.find(params[:id])
+      if @plant.update(plant_params)
+        render json: @plant
+      else
+        render json: @plant.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @plant = Plant.find(params[:id])
+      @plant.destroy
+    end
+
+    private
+
+    def plant_params
+      params.require(:plant).permit(:name, :description, :amount_of_sun, :days_to_water)
+    end
+  end
 end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,42 @@
 class SchedulesController < ApplicationController
+  class SchedulesController < ApplicationController
+    def index
+      @schedules = Schedule.all
+      render json: @schedules
+    end
+
+    def show
+      @schedule = Schedule.find(params[:id])
+      render json: @schedule
+    end
+
+    def create
+      @schedule = Schedule.new(schedule_params)
+      if @schedule.save
+        render json: @schedule, status: :created
+      else
+        render json: @schedule.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @schedule = Schedule.find(params[:id])
+      if @schedule.update(schedule_params)
+        render json: @schedule
+      else
+        render json: @schedule.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @schedule = Schedule.find(params[:id])
+      @schedule.destroy
+    end
+
+    private
+
+    def schedule_params
+      params.require(:schedule).permit(:plant_id, :user_id, :image_url, :watering_start_date)
+    end
+  end
 end
