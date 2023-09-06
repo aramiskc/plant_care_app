3bf04d6d6438e09092bf35436f392d1b305cf3d2
“Nickhil <suryanscrit@gmail.com>
2023-09-06 12:31:33 -0500

    working sessions route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  require "jwt"
+
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
